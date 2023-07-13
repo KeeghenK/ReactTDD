9c9b9d9957280586cd57ee48b05b25108f12135d
Keeghen Kessler <kjrkessler@gmail.com>
2023-07-13 17:36:09 -0500

    Test

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -18,6 +18,7 @@ function NoteForm(props) {
             name: e.target.value
           })
         }
+        value={formData.name}
         placeholder="Note Name"
       />
       <input
@@ -28,6 +29,7 @@ function NoteForm(props) {
             description: e.target.value
           })
         }
+        value={formData.description}
         placeholder="Note Description"
       />
       <button data-testid="note-form-submit" type="button" onClick={createNote}>
